00c5888d696fa2e8811423d50c41c17511113424
Lucjan Wiśnicz <luqapo@gmail.com>
2019-02-06 21:01:43 +0100

    dropdown skills

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburgerMenu = document.getElementById('hamburger');
     const menu = document.querySelector('.nav__menu');
     const menuListItems = document.querySelectorAll('li');
-    
-    
+    const skillsDropdownButton = document.querySelectorAll('i.fa-angle-down');
+    const skillsDropdownDiv = document.querySelectorAll('.skills__dropdown__item');
+
     hamburgerMenu.addEventListener('click', () => {
         menu.style.display = 'block';
         hamburgerMenu.style.display = 'none';
-    })
+    });
 
     menuListItems.forEach(item => {
         item.addEventListener('click', () => {
@@ -17,6 +18,21 @@ document.addEventListener("DOMContentLoaded", () => {
                 menu.style.display = 'none';
                 hamburgerMenu.style.display = 'block';
                 
+            }
+        })
+    });
+
+    skillsDropdownButton.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            if(skillsDropdownDiv[index].style.display === 'none'){
+                skillsDropdownDiv[index].style.display = 'block';
+                skillsDropdownDiv[index].style.opacity = 1;
+                skillsDropdownButton[index].classList.remove('fa-angle-down');
+                skillsDropdownButton[index].classList.add('fa-angle-up');
+            } else {
+                skillsDropdownDiv[index].style.display = 'none';
+                skillsDropdownButton[index].classList.remove('fa-angle-up');
+                skillsDropdownButton[index].classList.add('fa-angle-down');
             }
         })
     })
